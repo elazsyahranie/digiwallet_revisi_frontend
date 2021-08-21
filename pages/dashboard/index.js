99d987641dfb8987d3909c07d227ccc4c6f49661
elazsyahranie <elazaribrahims@gmail.com>
2021-08-21 16:54:44 +0700

    Dashboard rough finished

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -10,6 +10,7 @@ import axiosApiIntances from "/utils/axios";
 import Cookie from "js-cookie";
 import { connect } from "react-redux";
 import { getUserbyId } from "/redux/actions/user";
+import samuelSuhi from "/public/samuelSuhi.png";
 import dashboardIcon from "/public/grid_grey.png";
 import transferIcon from "/public/arrow-up.png";
 import topUpIcon from "/public/plus.png";
@@ -42,7 +43,7 @@ function Dashboard(props) {
             <Row>
               {/* LEFT MENU */}
               <Col lg={3} md={4} className="d-none d-md-block">
-                <div className={styles.whiteBackground}>
+                <div className={`${styles.whiteBackground} h-100`}>
                   <div className={`py-5`}>
                     <Button className={styles.leftMenuButtonSelected}>
                       <Image
@@ -105,31 +106,166 @@ function Dashboard(props) {
                   </Col>
                 </Row>
                 {/* RIGHT MENU BOTTOM */}
-                <Row className={`p-4 ${styles.whiteBackground}`}>
-                  <Col lg={9} md={9} sm={7} xs={7}>
+                <Row className="justify-content-between">
+                  <Col
+                    xl={6}
+                    lg={12}
+                    md={12}
+                    sm={12}
+                    xs={12}
+                    className={`p-4 h-100 mb-2 ${styles.whiteBackground}`}
+                  >
                     <Row>
-                      <div>
-                        <Image
-                          src={greenTopUpIcon}
-                          alt=""
-                          className="img-fluid"
-                        ></Image>
-                      </div>
-                      <span className="d-block">Income</span>
-                      <h5>Rp2.120.000</h5>
+                      <Col lg={8} md={8} sm={6} xs={6}>
+                        <Row>
+                          <div>
+                            <Image
+                              src={greenTopUpIcon}
+                              alt=""
+                              className="img-fluid"
+                            ></Image>
+                          </div>
+                          <span className="d-block">Income</span>
+                          <h5>Rp2.120.000</h5>
+                        </Row>
+                      </Col>
+                      <Col lg={4} md={4} sm={6} xs={6}>
+                        <Row>
+                          <div>
+                            <Image
+                              src={redIcon}
+                              alt=""
+                              className="img-fluid"
+                            ></Image>
+                          </div>
+                          <span className="d-block">Expense</span>
+                          <h5>Rp1.560.000</h5>
+                        </Row>
+                      </Col>
+                    </Row>
+                    <Row style={{ minHeight: "120px" }}>
+                      <h6 className="text-center my-auto">Unavaibale</h6>
                     </Row>
                   </Col>
-                  <Col lg={3} md={3} sm={5} xs={5}>
-                    <Row>
-                      <div>
-                        <Image
-                          src={redIcon}
-                          alt=""
-                          className="img-fluid"
-                        ></Image>
-                      </div>
-                      <span className="d-block">Expense</span>
-                      <h5>Rp1.560.000</h5>
+                  <Col
+                    xl={5}
+                    lg={12}
+                    md={12}
+                    sm={12}
+                    xs={12}
+                    className={`p-4 ${styles.whiteBackground}`}
+                  >
+                    <Row className={`mb-4`}>
+                      <Col lg={8} md={8} sm={8} xs={8}>
+                        <span className="fw-bold">Transaction History</span>
+                      </Col>
+                      <Col lg={4} md={4} sm={4} xs={4}>
+                        <span>See all</span>
+                      </Col>
+                    </Row>
+                    <Row className={`mb-4`}>
+                      <Col lg={3} md={3} sm={3} xs={3}>
+                        <div className="d-flex align-items-center">
+                          <Image
+                            src={samuelSuhi}
+                            alt=""
+                            className="img-fluid"
+                          ></Image>
+                        </div>
+                      </Col>
+                      <Col lg={5} md={5} sm={5} xs={5}>
+                        <Row>
+                          <span className="d-block fw-bold">Samuel Suhi</span>
+                          <span className="d-block">Transfer</span>
+                        </Row>
+                      </Col>
+                      <Col
+                        lg={3}
+                        md={3}
+                        sm={3}
+                        xs={3}
+                        className="d-flex align-items-center"
+                      >
+                        <span>See all</span>
+                      </Col>
+                    </Row>
+                    <Row className={`mb-4`}>
+                      <Col lg={3} md={3} sm={3} xs={3}>
+                        <div className="d-flex align-items-center">
+                          <Image
+                            src={samuelSuhi}
+                            alt=""
+                            className="img-fluid"
+                          ></Image>
+                        </div>
+                      </Col>
+                      <Col lg={5} md={5} sm={5} xs={5}>
+                        <Row>
+                          <span className="d-block fw-bold">Samuel Suhi</span>
+                          <span className="d-block">Transfer</span>
+                        </Row>
+                      </Col>
+                      <Col
+                        lg={3}
+                        md={3}
+                        sm={3}
+                        xs={3}
+                        className="d-flex align-items-center"
+                      >
+                        <span>See all</span>
+                      </Col>
+                    </Row>
+                    <Row className={`mb-4`}>
+                      <Col lg={3} md={3} sm={3} xs={3}>
+                        <div className="d-flex align-items-center">
+                          <Image
+                            src={samuelSuhi}
+                            alt=""
+                            className="img-fluid"
+                          ></Image>
+                        </div>
+                      </Col>
+                      <Col lg={5} md={5} sm={5} xs={5}>
+                        <Row>
+                          <span className="d-block fw-bold">Samuel Suhi</span>
+                          <span className="d-block">Transfer</span>
+                        </Row>
+                      </Col>
+                      <Col
+                        lg={3}
+                        md={3}
+                        sm={3}
+                        xs={3}
+                        className="d-flex align-items-center"
+                      >
+                        <span>See all</span>
+                      </Col>
+                    </Row>
+                    <Row className={`mb-4`}>
+                      <Col lg={3} md={3} sm={3} xs={3}>
+                        <div className="d-flex align-items-center">
+                          <Image
+                            src={samuelSuhi}
+                            alt=""
+                            className="img-fluid"
+                          ></Image>
+                        </div>
+                      </Col>
+                      <Col lg={5} md={5} sm={5} xs={5}>
+                        <Row>
+                          <span className="d-block fw-bold">Samuel Suhi</span>
+                          <span className="d-block">Transfer</span>
+                        </Row>
+                      </Col>
+                      <Col
+                        lg={3}
+                        md={3}
+                        sm={3}
+                        xs={3}
+                        className="d-flex align-items-center"
+                      >
+                        <span>See all</span>
+                      </Col>
                     </Row>
                   </Col>
                 </Row>
