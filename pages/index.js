20533e74bcff050bc3c3d2fa68841adf3aa7dde5
elazsyahranie <elazaribrahims@gmail.com>
2021-09-14 14:00:10 +0700

    Added navigation @ landing

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
+import Link from "next/link";
 import Layout from "/components/Layout";
 
 function LandingPage() {
+  const goToLogin = () => {
+    console.log("Go to login page!");
+  };
+
   return (
     <>
       <Layout title="Digiwallet">
         <h1>Landing Page</h1>
+        <div style={{ cursor: "pointer" }}>
+          <Link href="/login">
+            <h6>Login</h6>
+          </Link>
+        </div>
+        <div style={{ cursor: "pointer" }}>
+          <Link href="/dashboard">
+            <h6>Dashboard</h6>
+          </Link>
+        </div>
       </Layout>
     </>
   );
